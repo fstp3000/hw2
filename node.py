62886ff95f44c567ba2dd02ed4a65307d04a950c
Fstp <Fstp@FstpdeMacBook-Pro.local>
2019-05-06 12:11:47 +0800

    040

--- node.py
+++ node.py
@@ -6,7 +6,7 @@ from lib.common import cprint
 import json
 def start_node():
     with open('config.json') as f:
-        connfig = json.load(f)        
+        connfig = json.load(f)
     host = '0.0.0.0'
     port = config["p2p_port"]
     init_node()
@@ -33,7 +33,7 @@ def init_node():
         if len(txs) > len(transactions):
             txdb.clear()
             txdb.write(txs)
-    
+
 def get_nodes():
     return NodeDB().find_all()
 
@@ -51,8 +51,8 @@ def check_node(address):
     pass
 
 def rm_dup(nodes):
-    return sorted(set(nodes)) 
-    
+    return sorted(set(nodes))
+
 if __name__=='__main__':
     msg_send_Header = {"method": "sendHeader","data": {"block_hash": "0000be5b53f2dc1a836d75e7a868bf9ee576d57891855b521eaabfa876f8a606","block_header": "000000010000000008e647742775a230787d66fdf92c46a48c896bfbc85cdc8acc67e87d0000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000002321","block_height": 10 }}
     msg_get_Blocks = {"method":"getBlocks","data":{"hash_count":3,"hash_begin":"0000a1aff3716cb8925b18d2f6bb38d3f168d682c1218a70b503b5c600474e28","hash_stop":"0000913f8e974f30ac5f21a97ff2dcebf26b00d7fee92348454857260daaf60f"}}
@@ -64,22 +64,22 @@ if __name__=='__main__':
     #send_Header test
     response = rpc.BroadCast().router(msg_send_Header)
     print(json.dumps(response, indent = 4, sort_keys=True))
-    
+
     #get_Blocks test
     response = rpc.BroadCast().router(msg_get_Blocks)
     print(json.dumps(response, indent = 4, sort_keys=True))
-    
+
     #get_BlockCount test
     response = rpc.BroadCast().router(msg_get_BlockCount)
     print(json.dumps(response, indent = 4, sort_keys=True))
-    
+
     #get_BlockHash test
     response = rpc.BroadCast().router(msg_get_BlockHeader)
     print(json.dumps(response, indent = 4, sort_keys=True))
     #get_BlockHash err test
     response = rpc.BroadCast().router(err_msg_get_BlockHeader)
     print(json.dumps(response, indent = 4, sort_keys=True))
-    
+
     #getBlockHeader test
     response = rpc.BroadCast().router(msg_get_BlockHeader)
     print(json.dumps(response, indent = 4, sort_keys=True))
